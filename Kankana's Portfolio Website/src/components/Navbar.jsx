44f3b4a84e0ae6a8eb1c9b2c4309f1837540e117
Kankana Nath <kankana109@gmail.com>
2024-01-14 19:23:09 +0530

    Personal portfolio

---
+++ Kankana's Portfolio Website/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Navbar() {
+    const []
+  return (
+    <header>
+        <nav>
+            <div>
+                <div>
+                    <p className='logo'>Kankana</p>
+                </div>
+            </div>
+        </nav>
+    </header>
+  )
+}
+
+export default Navbar
