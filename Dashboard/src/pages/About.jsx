0a123ab3ab9dc2eb49d8f2c5d2d210e599bca845
Kankana Nath <kankana109@gmail.com>
2024-02-12 16:08:09 +0530

    Admin UI 1.0

---
+++ Dashboard/src/pages/About.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+import Box from '@mui/material/Box';
+import Navbar from '../components/Navbar'
+
+function About() {
+  return (
+    <>
+    <Navbar />
+    <Box height={30}/>
+    <Box sx={{ display: 'flex' }}>
+    <Sidebar/>
+    <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+    <h1>About</h1>
+    </Box>
+</Box>
+     
+    </>
+   
+  )
+}
+
+export default About
