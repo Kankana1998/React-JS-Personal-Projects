382996c7c852c6167ff9ae6f6c01918fa969f207
Kankana Nath <kankana109@gmail.com>
2024-02-12 12:17:39 +0530

    aa

--- Chat Application/src/utils/AuthContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext, useState, useEffect } from "react";
-
-const AuthContext = createContext()
-
-export const AuthProvider = ({children}) => {
-    return <AuthContext.Provider
-}
-
-export default AuthContext
