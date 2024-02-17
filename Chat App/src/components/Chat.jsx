b58e1563bbb78c83c2cee507c02508e814505e51
Kankana Nath <kankana109@gmail.com>
2024-02-17 23:54:13 +0530

    config change

--- Chat App/src/components/Chat.jsx
+++ Chat App/src/components/Chat.jsx
@@ -1,3 +1,24 @@
+import { useState } from "react";
+
 export const Chat = () => {
-    return <div className="chat-app">Chat</div>;
+
+const [newMessage, setNewMessage] = useState("");
+
+ const handleSubmit = () => {
+    e.preventDefault();
+    if(newMessage === "") return;
+    console.log(newMessage);
+ };
+
+    return <div className="chat-app">
+        <form className="new-message-form" onSubmit={handleSubmit}>
+    <input 
+    className="new-message-input" 
+    placeholder="Type your message here..." 
+    />
+    <button type="submit" className="send-button">
+        Send
+    </button>
+        </form> 
+        </div>;
 }
