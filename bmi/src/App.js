3851cf788b1fdc6cee93ca9a3197a5bace7f9bb0
Kankana Nath <kankana109@gmail.com>
2024-04-25 21:37:11 +0530

    bmi 2.0

--- bmi/src/App.js
+++ bmi/src/App.js
@@ -1,24 +1,46 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react';
+import './App.css';
 
 const App = () => {
+
+  const [height, setheight] = useState(70);
+  const [weight, setweight] = useState(180);
+
+function onWeightChange(event){
+  setweight(event.target.value)
+}
+
+function onHeightChange(event){
+  setheight(event.target.value)
+}
+
+const output = useMemo(() => {
+  const calculateHeight = height /100;
+  return (weight/(calculateHeight*calculateHeight)).toFixed(1)
+}, [weight, height]);
+
   return (
     <main>
     <h1>BMI Calculator</h1>
     <div className='input-selection'>
-      <p>Weight: 80kg</p>
+      <p>Weight: {weight} kg</p>
       <input className='input-slider'
       type='range'
       step='1'
       min='40'
       max='200'
+      onChange={onWeightChange}
       />
-      <p>Height: 180cm</p>
-      <input className='inpu-slider' 
+      <p>Height: {height} cm</p>
+      <input className='input-slider' 
       type='range'
+      min='140'
+      max='220'
+      onChange={onHeightChange}
       />
       <div className='output-section'>
         <p>YOUR BMI</p>
-        <p className='output'>100</p>
+        <p className='output'>{output}</p>
       </div>
     </div>
     </main>
